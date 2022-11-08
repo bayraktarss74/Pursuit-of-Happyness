ccd10ad9f991fb3c94cd2e61b0caa03d8cc2a09a
SefaSoner Bayraktar <sefasonerbayraktar4@gmail.com>
2022-11-08 22:28:44 +0300

    project created

---
+++ src/test/java/com/Upgenix/runners/CukesRunner.java
@@ -0,0 +1,25 @@
+package com.Upgenix.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+    },
+        features = "src/test/resources/features",
+        glue = "com/Upgenix/stepDefinitions",
+        dryRun = false,
+        tags = "Upgenix",
+        publish = false
+
+)
+public class CukesRunner {
+}
+
+
---
+++ src/test/java/com/Upgenix/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.Upgenix.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/cydeo/step_definitions",
+        features = "@target/rerun.txt"
+)
+public class FailedTestRunner {
+}
