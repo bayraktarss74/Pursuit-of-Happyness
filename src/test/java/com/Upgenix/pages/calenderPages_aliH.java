b8930f2361b8ecdeb10eb96452136da3cf650d23
alihelvacı <alihelvacioglu87@gmail.com>
2022-11-09 00:08:22 +0300

    PageFactory initialized

--- src/test/java/com/Upgenix/pages/calenderPages_aliH.java
+++ src/test/java/com/Upgenix/pages/calenderPages_aliH.java
@@ -1,4 +1,10 @@
 package com.Upgenix.pages;
 
+import com.Upgenix.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class calenderPages_aliH {
+    public calenderPages_aliH(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
