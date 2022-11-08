34f5d2ad2ebd3ecba54f3714643fc21014584d94
alihelvacı <alihelvacioglu87@gmail.com>
2022-11-08 23:38:40 +0300

    calenderPages_aliH.java created

--- src/test/java/com/Upgenix/runners/CukesRunner.java
+++ src/test/java/com/Upgenix/runners/CukesRunner.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
     },
         features = "src/test/resources/features",
         glue = "com/Upgenix/stepDefinitions",
