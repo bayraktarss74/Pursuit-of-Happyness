82b1644bc03fe6d98a917c800ba196356eb0dabc
alihelvacı <alihelvacioglu87@gmail.com>
2022-11-09 00:17:16 +0300

    class name updated

--- src/test/java/com/Upgenix/pages/calenderPage_aliH.java
+++ src/test/java/com/Upgenix/pages/calenderPage_aliH.java
@@ -3,8 +3,8 @@ package com.Upgenix.pages;
 import com.Upgenix.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class calenderPages_aliH {
-    public calenderPages_aliH(){
+public class calenderPage_aliH {
+    public calenderPage_aliH(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 }
