1fc9e99191255667e73f5fa3f373f909d9c546f5
alihelvacı <alihelvacioglu87@gmail.com>
2022-11-08 23:41:06 +0300

    .json deleted

--- src/test/java/com/Upgenix/runners/CukesRunner.java
+++ src/test/java/com/Upgenix/runners/CukesRunner.java
@@ -11,13 +11,13 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json",
+                "json:target/cucumber.json"
     },
         features = "src/test/resources/features",
         glue = "com/Upgenix/stepDefinitions",
         dryRun = false,
         tags = "",
-        publish = true
+        publish = false
 
 )
 public class CukesRunner {
