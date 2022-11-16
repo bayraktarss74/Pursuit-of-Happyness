1636af4384d0548eb88104a923ebfd97412226ef
selamigogsu <selamigogsu@outlook.com>
2022-11-16 23:01:38 +0300

    Emploees page plus

--- src/test/java/com/Upgenix/pages/EmployeesPage_Selami.java
+++ src/test/java/com/Upgenix/pages/EmployeesPage_Selami.java
@@ -1,4 +1,27 @@
 package com.Upgenix.pages;
 
+import com.Upgenix.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class EmployeesPage_Selami {
-}
+
+
+
+
+
+        public MainPage_Kadir(){
+            PageFactory.initElements(Driver.getDriver(),this);
+        }
+
+        @FindBy(id = "login")
+        public WebElement userNameInput;
+
+        @FindBy(id = "password")
+        public WebElement passwordInput;
+
+        @FindBy(xpath = "//a[@data-menu='157']")
+        public WebElement employeeLink;
+
+    }
