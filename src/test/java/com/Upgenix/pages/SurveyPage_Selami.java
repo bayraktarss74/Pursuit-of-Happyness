a1966d13730fc5b1a982677970b8a268721836e2
selamigogsu <selamigogsu@outlook.com>
2022-11-18 02:44:18 +0300

    SurveyPage_Selami

--- src/test/java/com/Upgenix/pages/SurveyPage_Selami.java
+++ src/test/java/com/Upgenix/pages/SurveyPage_Selami.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class EmployeesPage_Selami {
+public class SurveyPage_Selami {
     {
             PageFactory.initElements(Driver.getDriver(),this);
         }
