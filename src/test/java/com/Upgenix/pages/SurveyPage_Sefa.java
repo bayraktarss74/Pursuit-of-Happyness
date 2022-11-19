373b40a946eb9c126dac6dbf19adedc2b15ab63b
SefaSoner Bayraktar <sefasonerbayraktar4@gmail.com>
2022-11-19 12:12:45 +0300

    US2-3-4 work

--- src/test/java/com/Upgenix/pages/SurveyPage_Sefa.java
+++ src/test/java/com/Upgenix/pages/SurveyPage_Sefa.java
@@ -16,31 +16,51 @@ public class SurveyPage_Sefa {
 
     public SurveyPage_Sefa() {
 
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
 
     }
 
 
-        //locate the login button for username and password
+    //locate the login button for username and password
 // button for password
 // and login button
-        @FindBy(id = "login")
-        public WebElement email;
+    @FindBy(id = "login")
+    public WebElement email;
 
-        @FindBy(id = "password")
-        public WebElement passwordButton;
+    @FindBy(id = "password")
+    public WebElement passwordButton;
 
-        @FindBy(className = "btn btn-primary")
-        public WebElement logInButton;
+    @FindBy(className = "btn btn-primary")
+    public WebElement logInButton;
 
-        @FindBy(xpath = "//a[@data-menu='475']")
-        public WebElement surveyButton;
+    @FindBy(xpath = "//a[@data-menu='475']")
+    public WebElement surveyButton;
 
-        @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o-kanban-button-new']")
-        public WebElement createButton;
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o-kanban-button-new']")
+    public WebElement createButton;
 
-        @FindBy(className = "btn btn-primary btn-sm o_form_button_save")
-        public WebElement saveButton;
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/div[2]/button[1]")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[@class='o_notification_title']")
+    public WebElement errorMessage;
+
+    @FindBy (xpath = "(//*[contains(@id,'o_field_input')])[2]")
+    public WebElement titleField;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]/div[2]/div/p")
+    public WebElement surveyMessage;
 
 
-    }
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[1]/div[55]/div[2]/h4/span")
+    public WebElement createdName;
+
+
+    @FindBy (xpath ="//input[@class='o_searchview_input']")
+    public WebElement surveySearchInput;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]")
+    public WebElement createdSurvey;
+
+
+}
--- src/test/resources/features/883_Survey_Sefa.feature
+++ src/test/resources/features/883_Survey_Sefa.feature
@@ -1,5 +1,5 @@
 
-
+@sefawip
 Feature: As a Posmanager, I should be able to create, edit and delete surveys from "Surveys" module.
 
 
@@ -21,8 +21,29 @@ Feature: As a Posmanager, I should be able to create, edit and delete surveys fr
   @UPGN-883-US2
 
   Scenario: Verify that if Survey Title field leaves blank, an error message "The following fields are invalid:" is appeared.
-    When User is on the new survey page locates save button and clicks
-    Then User receives The following fields are invalid title.
+    When User locates surveys button on the screen and clicks on the surveys button
+    Then User locates create button on the screen and clicks on create button
+    Then User is on the new survey page locates save button and clicks
+    Then User receives The following fields are invalid message.
+
+    @UPGN-883-US3
+    Scenario: Verify that the "Survey created" message appears under the survey form sheet
+      When User locates surveys button on the screen and clicks on the surveys button
+      Then User locates create button on the screen and clicks on create button
+      Given User locates the title button and and sends key to the title section for a name
+      Then User locates save button and clicks on it
+      Then user locates the message below as "Survey created"
+
+  @UPGN-883-US4
+  Scenario: Verify that the user should be able to see created survey is listed after clicking the Surveys module.
+    When User locates surveys button on the screen and clicks on the surveys button
+    Then User locates create button on the screen and clicks on create button
+    Given User locates the title button and and sends key to the title section for a name
+    Then User locates save button and clicks on it
+    Then User locates surveys button on the screen and clicks on the surveys button
+    Then User fills Search Input with the created Survey's name and press Enter
+    Then User should see created survey is listed
+
 
 
 
